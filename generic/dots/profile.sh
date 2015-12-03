d036528aef4e6af66759c439caf56b5afb1e242e
Andrew Janke <andrew@apjanke.net>
2015-12-03 15:53:25 -0500

    bash: add RVM stuff
    This was added automatically by RVM when I installed it.

--- generic/dots/profile.sh
+++ generic/dots/profile.sh
@@ -11,3 +11,11 @@ if which subl &>/dev/null; then
    export EDITOR='subl -w'
    export VISUAL='subl -w'
 fi
+
+
+if [[ -e $HOME/.rvm/bin ]]; then
+  export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+  if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
+    source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+  fi
+fi
