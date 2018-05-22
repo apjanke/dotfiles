517bb44e5e29a640e0420ae4e984edba18ebdc6f
Andrew Janke <andrew@apjanke.net>
2018-05-22 04:32:58 -0400

    bashrc: add Homebrew completions

--- dotfiles/generic/bashrc.sh
+++ dotfiles/generic/bashrc.sh
@@ -5,7 +5,7 @@
 # Pull in common bash/zsh configuration
 if [ -f $HOME/.dotfiles/zshbashrc.zsh ]; then source $HOME/.dotfiles/zshbashrc.zsh; fi
 
-# bash-specific settings
+# bash-specific settings  #
 
 unset maybe_add_path
 
@@ -27,5 +27,14 @@ else
   export PS1="[\h: \W] \$ "
 fi
 
+#  Homebrew  #
+
+if type brew 2&>/dev/null; then
+  for completion_file in $(brew --prefix)/etc/bash_completion.d/*; do
+    source "$completion_file"
+  done
+fi
+
+#  Miscellaneous  #
 
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
