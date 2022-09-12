44b1f1297903bd24407463a042ad5ec29d53bc17
Andrew Janke <andrew@apjanke.net>
2022-09-11 20:08:54 -0400

    Tweak script implementations for my improved coding skills and taste

--- LICENSE.md
+++ LICENSE.md
@@ -1,3 +1,3 @@
-#  License  #
+# License
 
 All material in this project is licensed under the [MIT License](doc/MIT-LICENSE.txt) unless otherwise noted.
--- sys-setup/misc/setup-osx-misc.sh
+++ sys-setup/misc/setup-osx-misc.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #
-# Miscellaneous settings you might not want on all machines
-
+# Miscellaneous macOS settings you might not want on all machines.
 
 # Disable Notification Center and remove the menu bar icon
 launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
-
 
 # Disable smart quotes as they’re annoying when typing code
 #defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
